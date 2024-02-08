6699587c7f08213486c3c72eeb3936a070f538e7
Umbral1013 <Sixela121@hotmail.com>
2024-02-08 14:34:45 -0600

    Changed various things (mostly they were just renaming and stuff

--- python-pong.py
+++ python-pong.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """ TÍTULO DEL DOCUMENTO: Pong en Python 3 para principiantes.
-NOMBRE DEL ARCHIVO: pong_v2.py
+NOMBRE DEL ARCHIVO: python-pong.py
 AUTOR: Umbral1013.
 PROPÓSITO: Hacer una versión del Pong de @TokyoEdTech usando programación
     orientada a objetos.
@@ -12,6 +12,8 @@ FECHA: mié 03 ago 2022 21:00:18
 HISTORIAL DEL DOCUMENTO:
 	Versión - Fecha - Comentarios
     02 - 03/07/2022 - Pasar el código a POO.
+    03 - 08/02/2024 - Se ha renombrado el archivo, el sangrado de
+                        algunas secciones se ha cambiado.
 """
 
 import turtle
@@ -21,10 +23,12 @@ wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
+
 # Previene que la ventana se actualice. Ayuda con el rendimiento.
 wn.tracer(0)
 
 players = ["Jugador A", "Jugador B"]
+
 # El primero es el jugador A, el segundo es el jugador B.
 score = [0, 0]
 
@@ -59,12 +63,12 @@ class Paddle(Clay):
         # Características personalizadas.
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.goto(x_pos, y_pos)
-
+        
     def move_up(self):
         y = self.ycor()
         y += 20
         self.sety(y)
-
+        
     def move_down(self):
         y = self.ycor()
         y -= 20
@@ -78,7 +82,7 @@ class Ball(Clay):
         # Controlan la rapidez de la pelota.
         self.dx = -0.1
         self.dy = 0.1
-
+        
     def move(self):
         self.setx(self.xcor() + self.dx)
         self.sety(self.ycor() + self.dy)
@@ -90,7 +94,7 @@ class Text(Clay):
         Clay.__init__(self, tint)
         self.hideturtle()
         self.goto(0, 250)
-
+        
     def show_score(self):
         self.clear()
         self.write("{0}: {1} // {2}: {3}".format(
@@ -104,31 +108,27 @@ class Text(Clay):
 
 def collisions():
     # Colisiones entre la paleta y la pelota.
-    if (340 < ball.xcor() < 350) and (paddle_b.ycor() - 40 < ball.ycor() <
-                                      paddle_b.ycor() + 40):
-        ball.setx(340)
-        ball.dx *= -1
-        
-    if (-350 < ball.xcor() < -340) and (paddle_a.ycor() - 40 < ball.ycor() <
-                                        paddle_a.ycor() + 40):
-        ball.setx(-340)
-        ball.dx *= -1
+    if ((340 < ball.xcor() < 350) and
+		(second_player.ycor() - 40 < ball.ycor() < second_player.ycor() + 40)):
+            ball.setx(340)
+            ball.dx *= -1
+    if ((-350 < ball.xcor() < -340) and
+		(first_player.ycor() - 40 < ball.ycor() < first_player.ycor() + 40)):
+            ball.setx(-340)
+            ball.dx *= -1
 
 def borderChecking():
     if ball.ycor() > 290:
         ball.sety(290)
         # Invierte la dirección al tocar el muro superior.
         ball.dy *= -1
-
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-
     if ball.xcor() > 390:
         service()
         score[0] += 1
         scoreboard.show_score()
-
     if ball.xcor() < -390:
         service()
         score[1] += 1
@@ -139,35 +139,34 @@ def service():
     ball.dx *= -1
 
 def checkDistance():
-    """ Revisa si la distancia entre la pelota y la raqueta es mayor a 25
-    píxeles.
-    """
-    if abs(paddle_b.ycor() - ball.ycor()) > 25:
+    """ Revisa si la distancia entre la pelota y la raqueta es mayor a 
+    25 píxeles. """
+    if abs(second_player.ycor() - ball.ycor()) > 25:
         return True
 
 def robot():
-    """ Revisa si la raqueta está en la misma coordenada 'y' que la pelota. Si
-    no lo está, mueve la raqueta hacia allá.
-    """
-    if (paddle_b.ycor() < ball.ycor()) and checkDistance():
-        paddle_b.move_up()
-    elif (paddle_b.ycor() > ball.ycor()) and checkDistance():
-        paddle_b.move_down()
+    """ Revisa si la raqueta está en la misma coordenada 'y' que la 
+    pelota. Si no lo está, mueve la raqueta hacia allá. """
+    if (second_player.ycor() < ball.ycor()) and checkDistance():
+        second_player.move_up()
+    elif (second_player.ycor() > ball.ycor()) and checkDistance():
+        second_player.move_down()
 
 # Creando instancias.
-paddle_a = Paddle(-350)
-paddle_b = Paddle(350)
+first_player = Paddle(-350, 20)
+second_player = Paddle(350, 35)
 ball = Ball()
 scoreboard = Text("Yellow")
 scoreboard.show_score()
 
 # Asignación de teclas.
 wn.listen()
-# Cuando el usuario presiona "w", la función paddle_a_up es llamada.
-wn.onkeypress(paddle_a.move_up, "w")
-wn.onkeypress(paddle_a.move_down, "s")
-wn.onkeypress(paddle_b.move_up, "Up")
-wn.onkeypress(paddle_b.move_down, "Down")
+
+# Cuando el usuario presiona "w", la función first_player_up es llamada.
+wn.onkeypress(first_player.move_up, "w")
+wn.onkeypress(first_player.move_down, "s")
+wn.onkeypress(second_player.move_up, "Up")
+wn.onkeypress(second_player.move_down, "Down")
 
 while True:
     # Actualiza la pantalla cada vez que el bucle corre.
